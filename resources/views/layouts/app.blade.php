8ea7ee5aa90f2fd36aa5e29dc6e26ffc2330e30d
lluca1 <ludosanuluca@gmail.com>
2025-04-05 00:47:21 +0300

    redundancy removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,6 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -33,6 +32,5 @@
                 @yield('content')
             </main>
         </div>
-        @livewireScripts
     </body>
 </html>
