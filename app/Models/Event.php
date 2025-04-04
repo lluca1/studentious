d277aa21f8e39e14138aff6ee04d00f87aa771b3
lluca1 <ludosanuluca@gmail.com>
2025-04-05 00:46:03 +0300

    event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,49 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'start_time',
+        'end_time',
+        'creator_id'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+
+    /**
+     * Get the user that created the event.
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    /**
+     * Get the users attending this event.
+     */
+    public function attendees()
+    {
+        return $this->belongsToMany(User::class, 'event_user');
+    }
 }
