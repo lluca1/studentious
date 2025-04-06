ecc039471ec3d0d3a3fd3cc8c6fa1c008c10cfce
Drag0sh7 <rosudragos2007@gmail.com>
2025-04-06 03:23:13 +0300

    tag seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Event;
+use App\Models\Tag;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -18,19 +19,28 @@ class UserSeeder extends Seeder
             'password' => Hash::make('password'),
         ]);
 
-        // Give the demo user some events
+        // Give the demo user 3 events
         Event::factory()->count(3)->create([
             'creator_id' => $defaultUser->id,
         ]);
 
-        // Create 5 more users and assign each random events
+        // Attach random tags to the demo user
+        $defaultUser->tags()->attach(Tag::inRandomOrder()->take(3)->pluck('id'));
+
+        // Create 5 additional users
         User::factory()
             ->count(5)
             ->create()
             ->each(function ($user) {
+                // Each user gets 2–4 events
                 Event::factory()->count(rand(2, 4))->create([
                     'creator_id' => $user->id,
                 ]);
+
+                // Each user gets 1–3 tags
+                $user->tags()->attach(
+                    Tag::inRandomOrder()->take(rand(1, 3))->pluck('id')
+                );
             });
     }
 }
