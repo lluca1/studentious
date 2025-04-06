962479a9b16049d197b062be5a867323a916f63b
lluca1 <ludosanuluca@gmail.com>
2025-04-06 12:25:11 +0300

    login correction

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
         <div class="flex items-center justify-end mt-4">
             <div class="flex flex-col items-end space-y-2">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
-                    {{ __('Already have an account?') }}
+                    {{ __('Register') }}
                 </a>
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
